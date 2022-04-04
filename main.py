039bf43da6b37949b4c0ed028d8c89e6115443b3
Movie Hub Bots ✓ <99673503+TEAM-MOVIE-HUB-BOTS@users.noreply.github.com>
2022-04-04 11:16:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ START_BUTTON = InlineKeyboardMarkup(
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, message):
-    await message.reply_sticker("CAACAgUAAxkBAAEBcr1hsLH3Nu0-qQpwwWQ7FkF58xnwSgACpAMAAjieoFU-Q-udLfwBUx4E")
+    await message.reply_sticker("CAACAgIAAxkBAAECOJBiSoX_xwmX7iTp4BB3Hh5cSxf5LgACTRYAAnJ3wEiqLAI1EPSbGR4E")
     await message.reply_text(
         f""" Hai {message.from_user.mention} am Service Message, command and link deleter bot.""", 
         disable_web_page_preview=True,
